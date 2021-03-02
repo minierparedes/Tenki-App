4f3b76eb9e4a069083b994bb89f1e5f93d590062
minierparedes <3thancr0wn@gmail.com>
2021-03-02 11:58:43 +0900

    Initial Commit

---
+++ Tenki App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tenki App
+//
+//  Created by ethancr0wn on 2021/03/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tenki App/Tenki_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tenki_AppApp.swift
+//  Tenki App
+//
+//  Created by ethancr0wn on 2021/03/02.
+//
+
+import SwiftUI
+
+@main
+struct Tenki_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
